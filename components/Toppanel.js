f6edc41da65f50120c861e5703023fbe833a0e69
funbunch <shannan_bunch@hotmail.com>
2022-10-10 12:44:20 -0700

    added new mob images. reworked grid layout. still fixing resize issues.

--- components/Toppanel.js
+++ components/Toppanel.js
@@ -2,10 +2,14 @@ import Link from 'next/link'
 import Image from 'next/image'
 import styled from 'styled-components'
 import topPanelMob from '../public/img/bladetop-mob@2x@2x.jpg'
-import topPanel from '../public/img/toppanel2.jpg'
-import systemLogos from '../public/img/System-Logo@2x.png'
+import topPanel from '../public/img/toppanel3.jpg'
+import ps5 from '../public/img/ps5@2x.png'
+import xbox from '../public/img/xbox@2x.png'
 import phone from '../public/img/phone@2x.png'
+import ps5Mob from '../public/img/ps5-mob@2x.png'
+import xboxMob from '../public/img/xbox-mob@2x.png'
 import ctaVideo from '../public/img/cta-watch-video@2x.png'
+import ctaVideoMob from '../public/img/cta-video-mob@2x.png'
 import appleBadge from '../public/img/Apple-Store-Badge@2x.png'
 import googleBadge from '../public/img/Google-Play-Badge@2x.png'
 
@@ -18,34 +22,54 @@ export const Toppanel = () => {
 // `
 return (
   <div className='relative'> 
-    <div className="md:hidden">
+    <div className="md:hidden relative">
       <Image src={topPanelMob} width={768} height={1946} />
     </div> 
-    <div className='hidden md:block'>
+    <div className='hidden md:block relative'>
       <Image src={topPanel} width={1920} height={913} />
     </div>  
-    <div className="content px-12">
-      <div className="left-col absolute top-12 z-40 md:top-40 md:text-left md:left-[358px]">
-        <div className="system-logos absolute top-10 md:top-6 z-40">
-          <Image src={systemLogos} width={272} height={33} />
+    <div className="content absolute px-12 top-3 md:left-[17%] md:top-[60px] md:w-full md:max-w-[600px]">
+      <div className="content-col z-40">
+        <div className="system-logos-mob md:hidden">
+          <div className="ps5 inline-block pr-5">
+              <Image src={ps5Mob} width={44} height={15} />
+          </div>
+          <div className="xbox inline-block">
+            <Image src={xboxMob} width={61} height={19} />
+          </div>
         </div>
-        <h2 className="md:pb-10 md:text-[55px] leading-[1.2]">Relive Your Gaming<br />Greatness Again<br /> and Again
+        <div className="system-logos hidden md:block">
+          <div className="ps5 inline-block pr-11">
+              <Image src={ps5} width={118} height={27} />
+          </div>
+          <div className="xbox inline-block">
+            <Image src={xbox} width={107} height={33} />
+          </div>
+        </div>
+        <h2 className="pt-11 pb-7 md:pb-10 md:text-[55px] leading-[1.2]">Relive Your Gaming<br />Greatness Again<br />and Again
         </h2>
-        <p className="text-base md:pb-10">Your greatest gaming moments shouldn't be fleeting. They<br />should be saved and savored like fine wine and fancy cheeses.<br />All you have to do is connect the Postparty mobile app to your<br />PS5 or XBOX to capture your most awesome moments, build a<br />vast collection of your greatest hits and share them via social<br />media with just a few clicks.
+        <p className="text-base md:pb-10">Your greatest gaming moments shouldn't be fleeting. They should be saved and savored like fine wine and fancy cheeses. All you have to do is connect the Postparty mobile app to your PS5 or XBOX to capture your most awesome moments, build a vast collection of your greatest hits and share them via social media with just a few clicks.
         </p>
-        <div className="cta pb-12">
+        <div className="cta-mob text-center md:hidden pt-7 pb-12">
           <a href="">
-          <Image src={ctaVideo} width={195} height={50} />
+          <Image src={ctaVideoMob} width={134} height={32} />
           </a>
         </div>
+        <div className="cta hidden md:block pb-12">
+          <a href="">
+          <Image src={ctaVideo} width={195} height={50} />
+        </a>
+        </div>
+        <div className="badge w-[30%]">
         <div className="apple pr-7 inline-block">
           <Image src={appleBadge} width={161} height={53} />
         </div>
         <div className="google inline-block">
-          <Image src={googleBadge} width={182 } height={54} />
+          <Image src={googleBadge} width={182} height={54} />
+        </div>
         </div>
       </div>
-      <div className="phone absolute top-[35rem] left-[40px] md:top-[9rem] md:left-[62%]">
+      <div className="phone md:hidden absolute top-[35rem] left-[40px]">
         <Image src={phone} width={335} height={590} layout='intrinsic' />
       </div>
     </div>
