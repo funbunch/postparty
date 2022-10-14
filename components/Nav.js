444924aed6a531372c1bc481bd9abd3c6a0fc672
funbunch <shannan_bunch@hotmail.com>
2022-10-14 16:39:22 -0700

    Solved Nav height issue

--- components/Nav.js
+++ components/Nav.js
@@ -10,71 +10,71 @@ const Nav = () => {
   const [navbar, setNavbar] = useState(false);
 
   return (
-    <nav className="w-full relative md:h-[70px] md:flex md:items-center ">
-    <div className="justify-between md:w-full md:items-center md:flex md:pl-8">
-        <div className="flex items-center justify-between md:py-5 md:block">
-          <a className="flex items-center py-2.5 md:py-0 pl-5" href="#">
-          <Image src={logo} alt="postparty Logo" width={214} height={41} />
-          </a>
-          <div className="md:hidden">
-            <button
-              className="menu-btn p-2 text-white outline-none focus:outline-none"
-              onClick={() => setNavbar(!navbar)}
-            >
-              {navbar ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-8 h-8 text-white m-auto"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-8 h-8 text-white m-auto"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-              )}
-            </button>
-          </div>
+    <nav className="w-full md:h-[70px] md:flex md:items-center ">
+    <div className="justify-between md:w-full md:h-full md:items-center md:flex md:pl-8">
+      <div className="flex items-center justify-between md:block">
+        <a className="flex items-center py-2.5 md:py-0 pl-5" href="#">
+        <Image src={logo} alt="postparty Logo" width={214} height={41} />
+        </a>
+        <div className="md:hidden">
+          <button
+            className="menu-btn p-2 text-white outline-none focus:outline-none"
+            onClick={() => setNavbar(!navbar)}
+          >
+            {navbar ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-8 h-8 text-white m-auto"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-8 h-8 text-white m-auto"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            )}
+          </button>
         </div>
-        <div
-          className={`flex-1 justify-self-center md:block md:pb-0 md:mt-0 ${
-            navbar ? 'block' : 'hidden'
-          }`} 
-        >
-          <ul className="menu md:bg-transparent items-center md:flex md:justify-center text-sm md:text-base pt-2 md:pt-0 tracking-wide">
-            <li className="border-b border-btext-white uppercase py-2.5 pl-7 md:hidden">
-              <Link href="/">
-                <a>Home</a>
-              </Link>
-            </li>
-            <li className="border-b md:border-none text-white uppercase py-2.5 pl-7 md:pl-28">
-              <Link href="/faq">
-                <a>FAQ</a>
-              </Link>
-            </li>
-            <li className="text-white uppercase h-8"></li>
-            <li className="bg-[#278eff] md:bg-[#500096] md:ml-auto md:px-14 py-5 text-center text-white uppercase">
-              <a>Download</a>
-            </li>  
-          </ul>
-        </div>
+      </div>
+      <div
+        className={`flex-1 justify-self-center md:block md:h-full ${
+          navbar ? 'block' : 'hidden'
+        }`} 
+      >
+        <ul className="menu md:bg-transparent items-center md:flex md:justify-center text-sm md:text-base pt-2 md:p-0 md:h-full tracking-wide">
+          <li className="border-b border-btext-white uppercase py-2.5 pl-7 md:hidden">
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li className="border-b md:border-none text-white uppercase py-2.5 pl-7 md:pl-28">
+            <Link href="/faq">
+              <a>FAQ</a>
+            </Link>
+          </li>
+          <li className="text-white uppercase h-8"></li>
+          <li className="bg-[#278eff] md:bg-[#500096] md:ml-auto md:px-14 md:h-full py-5 text-center text-white uppercase">
+            <a>Download</a>
+          </li>  
+        </ul>
+      </div>
     </div>
   </nav>
   )
