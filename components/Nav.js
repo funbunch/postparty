ea600769b61c0681490de543deb39210bd02d090
funbunch <shannan_bunch@hotmail.com>
2022-10-20 11:17:00 -0700

    Fixed active nav state.

--- components/Nav.js
+++ components/Nav.js
@@ -68,8 +68,8 @@ const Nav = () => {
             </Link>
           </li>
           <li className="border-b md:border-none text-white uppercase py-2.5 pl-7 md:pl-20">
-            <Link href="/faq" className={router.pathname == "/faq" ? "active" : ""} passHref>
-              <a>FAQ</a>
+            <Link href="/faq" passHref>
+              <a className={router.pathname == "/faq" ? "active" : ""}>FAQ</a>
             </Link>
           </li>
           <li className="text-white uppercase h-8"></li>
